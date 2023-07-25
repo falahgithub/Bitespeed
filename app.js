67f233ed854f93b78cb7370ab431b683d34a515d
falah khan <falahdelli5@gmail.com>
2023-07-25 12:12:37 +0530

    A function Create gets created that checks whether data exist in the database or not. If exists, keeps track of primary account id.

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+var countID = 1;
+const allEmails = [];
+const allnumbers = [];
+
+const app = express();
+app.use(express.json());
+
+
+////////////////////////////////////////////////////////////////////
+mongoose.connect("mongodb://127.0.0.1:27017/ContactDB");
+
+const contactSchema = new mongoose.Schema(
+  {
+    _id: { type: Number, default: 1 },
+    phoneNumber: String,
+    email: String,
+    linkedId: Number,
+    linkPrecedence: {
+      type: String,
+      enum: {
+        values: ["secondary", "primary"],
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const Contact = mongoose.model("Contact", contactSchema);
+
+Contact.find().then((data) => {
+  data.forEach(function (object) {
+    allEmails.push(object.email);
+    allnumbers.push(object.phoneNumber);
+  });
+});
+
+async function SAVE(email, number, linkPrecedence, linkedId) {
+    contact = new Contact({
+      phoneNumber: number,
+      email: email,
+      linkPrecedence: linkPrecedence,
+      linkedId: linkedId,
+      _id: countID,
+    });
+    allEmails.push(email);
+    allnumbers.push(number);
+    countID++;
+    await contact.save();
+  };
+  
+  SAVE(
+    (email = "lorraine@hillvalley.edu"),
+    (number = "123456"),
+    (linkPrecedence = "primary"),
+    (linkedId = null)
+  );
+
+
+////////////////////////////////////////////////////////////////////////
+
+async function Create(email, phoneNumber) {
+    await Contact.find({ $and: [{ email: email }, { phoneNumber: phoneNumber }] })
+      .then(async function (data) {
+        // ----------------------------------------------------- duplication -------
+        if (data.length != 0) {
+        } // data exists so do nothing
+  
+        //-------------------------------------------------------- new entry --------
+        else {
+  
+          // finding primary id
+  
+          var idtobekeptprimary = null;
+          await Contact.find({ $or: [{ email: email }, { phoneNumber: phoneNumber }] })
+            .sort({ createdAt: 1 })
+            .then((data) => {
+              if (data.length != 0) {
+                  if(data[0].linkedId == null) {idtobekeptprimary = data[0]._id}
+                else {idtobekeptprimary = data[0].linkedId; }   };
+            });
+        }
+    });
+};
+  
+
+
+
+
+app.listen(3000 || process.env.PORT, function () {
+    console.log("Server started on port 3000.");
+  });  
